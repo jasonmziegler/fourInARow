03980ae9b66648f455ebc7ef2c6ab4bd2ec29bfd
jasonmziegler <jason@insightfulautomation.com>
2018-11-26 10:54:46 -0600

    add board constructor, createSpaces Method, and Space Class constructor

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,26 @@
 class Board {
-    
+    constructor() {
+    	this.rows = 6,
+    	this.columns = 7,
+    	this.spaces = createSpaces();
+
+    }
+
+    createSpaces() {
+    	const newSpacesArray = [];
+
+    	for (let x=0;x<this.rows;x++) {
+    		const column = [];
+
+    		for (let y=0;y<this.columns;y++){
+    			const space = new Space(x,y);
+    			column.push(space);
+    		}
+
+    		newSpaceArray.push(column);
+
+    	}
+
+    	return newSpaceArray;
+    }
 }
