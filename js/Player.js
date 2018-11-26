81c56dd876fea20c1c1df43ee9bff183ba565de4
jasonmziegler <jason@insightfulautomation.com>
2018-11-25 23:16:49 -0600

    add Player Class Constructor and createTokens player method

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,20 @@ class Player {
       this.id = id,
       this.color = color,
       this.active = false,
-      this.tokens = [];
+      this.tokens = createTokens(21);
+    }
+
+    /**
+ 	* Creates token objects for player
+ 	* @param     {number}    num - Number of token objects to be created
+ 	* @returns   {Array}     An array of the newly created token objects
+	 */
+    createTokens(num) {
+    	 const newTokenArray = [];
+	 	for (let i= 0; i < num; i++){
+	 		let token = new Token(i, this);
+	 		newTokenArray.push(token)
+	 	}
+	 	return newTokenArray;
     }
 }
